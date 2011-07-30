233018f55c903ea9dc409198a614269b637445b5
Sai <saikiran.mothe@gmail.com>
2011-07-30 11:14:43 +0530

    new app

---
+++ config/initializers/secret_token.rb
@@ -0,0 +1,7 @@
+# Be sure to restart your server when you modify this file.
+
+# Your secret key for verifying the integrity of signed cookies.
+# If you change this key, all old signed cookies will become invalid!
+# Make sure the secret is at least 30 characters and all random,
+# no regular words or you'll be exposed to dictionary attacks.
+Gap::Application.config.secret_token = '7bfa6f68ff460412790d1b67c55b75538bcae1dfee40bf87a911466869031679cdb380688f9e644782d1c56e48330a10d5c03112b2ad98549b9682f5f1a49cca'
